e6ea69b566a3e4cce4e44f5da49120631ee0633c
Kushal Ghule <kaghule77@gmail.com>
2022-06-21 12:24:11 +0530

    Add project files.

---
+++ SampleMVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleMVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string[] courses = { "C#", "ASP.NET", "MVC", "Angular" };
+            ViewData["Message"] = "This is my first mvc app";
+            ViewBag.Courses= courses; 
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult ProductList()
+        {
+            string[] course = { "C#", "ASP.NET", "MVC", "Angular" };
+            ViewData["Message"] = "This is my first mvc app";
+            return View();
+        }
+    }
+}
---
+++ SampleMVCApplication/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMVCApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            ViewData["Message"] = "This is my first mvc app";
+            return View();
+        }
+    }
+}
